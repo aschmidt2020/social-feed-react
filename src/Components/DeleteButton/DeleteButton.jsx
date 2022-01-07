3a58fdea1b2982b78a23882ba9fb8b8f0e1ff81d
Audrey Schmidt <audsch@outlook.com>
2022-01-07 10:19:27 -0600

    changed formatting of delete confirm alert box

--- src/Components/DeleteButton/DeleteButton.jsx
+++ src/Components/DeleteButton/DeleteButton.jsx
@@ -2,7 +2,7 @@ const DeleteButton = (props) => {
     
     function handleClick(event){
         event.preventDefault();
-        let confirmDelete = window.confirm('Are you sure you would like to delete this post? Select OK for yes or cancel to cancel.')
+        let confirmDelete = window.confirm(`Are you sure you would like to delete this post?\n\nName of user: '${props.entry.user}'\nPost content: '${props.entry.post}'\n\nSelect OK for yes or Cancel to cancel.`)
         if(confirmDelete){
             props.deleteEntry(props.entry); 
         }
