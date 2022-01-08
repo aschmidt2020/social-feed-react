2fd62fe684d4087172728b11cee203f3ffc0f4c3
Audrey Schmidt <audsch@outlook.com>
2022-01-08 10:46:14 -0600

    fixed formatting of edit/delete buttons on comments

--- src/Components/ReplySection/ReplySection.jsx
+++ src/Components/ReplySection/ReplySection.jsx
@@ -9,13 +9,13 @@ const ReplySection = (props) => {
                 return (
                     <li key={reply.replyTimeStamp}>
                         <div className='row border-box-comments' style={{'marginBottom': '1em'}}>
-                            <div className="col-10">
+                            <div className="col-9">
                                 <span className='comment-name'>{reply.name}</span>
                                 <br></br>
                                 <span className='comment-text'>{reply.reply}</span>
                             </div>
 
-                            <div className="col-2" style={{'marginTop': '0.5em'}}>
+                            <div className="col-3" style={{'marginTop': '0.5em'}}>
                                 <span><EditReply editReply={props.editReply} entry={props.entry} reply={reply} replies={props.replies}/></span>
                                 <span><DeleteReply deleteReply={props.deleteReply} entry={props.entry} reply={reply} replies={props.replies}/> </span>
                             </div>
